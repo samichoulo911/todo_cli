9e3e0d551af786ac2aaf22597d082443e20d7181
Samer Massaad <samer.massaad@gmail.com>
2024-11-02 01:54:09 +0000

    Add support for xdg compliant config

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,16 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+enum DatabaseSource {
+    File(String),
+    Git(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Database {
+    source: DatabaseSource,
+}
+
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct TodoElement {
     pub completed: bool,
@@ -12,16 +23,39 @@ pub struct TodoDataBase {
     pub elements: Vec<TodoElement>,
 }
 
-const FILE_DB: &str = "todo.json";
-pub fn get_db() -> TodoDataBase {
-    if let Ok(content) = std::fs::read_to_string(FILE_DB) {
-        if !content.is_empty() {
-            return serde_json::from_str(&content).unwrap()
-        }
+fn get_db_config() -> Option<Database> {
+    let xdg_directory = xdg::BaseDirectories::with_prefix("todo_cli").unwrap();
+    let config_path = xdg_directory
+        .place_config_file("database_config.json")
+        .expect("Failed to create xdg config file");
+    if let Ok(config_content) = std::fs::read_to_string(config_path) {
+        return Some(serde_json::from_str(&config_content).unwrap())
     }
+    None
+}
+
+pub fn get_db() -> TodoDataBase {
+    let db_config = get_db_config();
+    match db_config.unwrap().source {
+        DatabaseSource::File(file_name) => {
+            if let Ok(content) = std::fs::read_to_string(file_name) {
+                if !content.is_empty() {
+                    return serde_json::from_str(&content).unwrap();
+                }
+            }
+        },
+        DatabaseSource::Git(_) => todo!()
+    }
+
     TodoDataBase::default()
 }
 
 pub fn save_db(db: TodoDataBase) {
-    let _ = std::fs::write(FILE_DB, serde_json::to_string(&db).unwrap());
+    let db_config = get_db_config();
+    match db_config.unwrap().source {
+        DatabaseSource::File(file_name) => {
+            let _ = std::fs::write(file_name, serde_json::to_string(&db).unwrap());
+        },
+        DatabaseSource::Git(_) => todo!()
+    }
 }
